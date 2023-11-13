a554e513b821dee0d363bd4dcebd4b6728e362e0
Muhammad Fakhri Hidayat <63569919+fakhrihiday17@users.noreply.github.com>
2023-11-14 01:35:24 +0700

    Update Bootstrap

--- resources/views/indexwf.blade.php
+++ resources/views/indexwf.blade.php
@@ -7,14 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aplikasi Buku Novel</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/style.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/bootstrap.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5/10/0/css/all.css">
-    <link rel="stylesheet" href="node_modules/swiper/swiper-bundle.min.css" />
-    <script src="node_modules/swiper/swiper-bundle.min.js"></script>
-    <script type="text/javascript" src="{{ asset('assets') }}/js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="{{ asset('assets') }}/js/bootstrap.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}">
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
 
     <title>Document</title>
 </head>
